658edbc41c20f2dcce9ceab36d61837d48bcde52
ARANCIBIA Pablo (pablo.arancibia) <pablo.arancibia@chaco.gob.ar>
2023-07-12 22:34:40 +0200

    permissions for list files

--- propylon_document_manager/file_versions/api/views.py
+++ propylon_document_manager/file_versions/api/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, CreateModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.response import Response
 
 from file_versions.models import FileVersion
 from .serializers import FileVersionSerializer
 
-# class FileVersionViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
-class FileVersionViewSet(ModelViewSet):
-    #authentication_classes = []
-    #permission_classes = []
+class FileVersionViewSet(CreateModelMixin, RetrieveModelMixin, ListModelMixin, GenericViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = FileVersionSerializer
     queryset = FileVersion.objects.all()
-    #lookup_field = "id"
+    lookup_field = "id"
+
+    def list(self, request):
+        filesVersion = FileVersion.objects.filter(file_user=request.user)
+        serializer = self.serializer_class(filesVersion, many=True)
+        return Response(serializer.data)
